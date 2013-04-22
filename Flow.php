e78a48f8e181ffe0d22bdf8539f4b6b18d9c09f1
SunIn <dengchenhua@gmail.com>
2013-04-22 13:46:15 +0800

    Create Flow.php

---
+++ Flow.php
@@ -0,0 +1,49 @@
+<?php
+  header('Content-type: text/json');
+	require_once('phpinc/global.php');
+	$arr = $_REQUEST;
+	$typeRank = $arr['typeRank'];
+	$typeSize = $arr['typeSize'];
+	$name = 	$arr['name'];
+	$tag = $arr['tag'];
+	$queryType = $arr['queryType']; 
+
+	$sql = '';
+	if(isset($name)&&$name!=0) {
+		$strName = " and name=".$name;
+	} else {
+		$strName ="";
+	}
+	if(isset($tag)) {
+		$strTag = " and tag like '%".$tag."%' ";
+	}else{
+		$strTag = "";
+	}
+	if(isset($typeRank)) {
+		$strRank = " order by ".$typeRank;
+	}else{
+		$strRank = "";
+	}
+	if(isset($queryType)) {
+		switch ($queryType) {
+			case 'list':
+				$sql = "select * from `petpic` where typeSize=".$typeSize.$strName.$strRank." desc limit 0,20";
+				break;
+			case 'tag':
+				$sql="select * from `petpic` where typeSize=".$typeSize.$strName.$strTag.$strRank." desc limit 0,20";
+				break;			
+			case 'flow':
+				$queryIndex = $arr['queryIndex'];
+				$from = $queryIndex*5;
+				$sql = "select * from `petpic` where typeSize=".$typeSize.$strName.$strTag.$strRank." desc limit ".$from.",20";
+				break;	
+			default:
+				break;
+		}
+	}
+	//echo $sql;
+	$BaiDuImage->Db_Query($sql);
+	$re=$BaiDuImage->Db_Fetch_All();
+	echo json_encode($re);	
+	exit();
+?>
